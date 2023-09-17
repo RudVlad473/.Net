5429102e97e90e0e06821770607e34819d7bbac7
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-09-17 12:48:39 +0300

    add accounts mocks

---
+++ Lab 1/ATM/Class Library/Mocks/MockAccounts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library
+{
+    public class MockAccounts
+    {
+        public static List<Account> GetMockAccounts()
+        {
+            return new List<Account>
+                {
+                    new Account("1", "John", "Doe", 5000, "1234"),
+                    new Account("2", "Alice", "Smith", 3500, "1234"),
+                    new Account("3", "Bob", "Johnson", 10000, "1234"),
+                };
+        }
+    }
+}
