ff5810d81d95b819b341984ca560033543788409
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-09-21 15:49:13 +0300

    add util classes

---
+++ Lab 1/ATM/Class Library/Utils/TextFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library.Utils
+{
+    public static class TextFormatter
+    {
+        public static string SplitCamelCase(string input)
+        {
+            return string.Concat(input.Select(x => Char.IsUpper(x) ? " " + x : x.ToString()))
+                .TrimStart(' ');
+        }
+
+        public static string UnsplitCamelCase(string input)
+        {
+            return input.Replace(" ", "");
+        }
+    }
+}
