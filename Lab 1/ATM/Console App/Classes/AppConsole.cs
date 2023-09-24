140a0036d8ff2d109e3299aeb7a7c86f31eca59b
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-09-24 14:54:23 +0300

    console app

---
+++ Lab 1/ATM/Console App/Classes/AppConsole.cs
@@ -0,0 +1,178 @@
+﻿using Class_Library;
+using Class_Library.Utils;
+using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_App.Classes
+{
+    public static class AppConsole
+    {
+        public static string GetClassListAsTable<T>(List<T> instances, bool shouldEnumerate = false)
+        {
+            if (instances == null || instances.Count == 0)
+            {
+                return "";
+            }
+
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+
+            // Extract property names as column headers
+            var columns = properties
+                .Where(property => property.CanRead && !property.GetMethod.IsStatic)
+                .Select(property => TextFormatter.SplitCamelCase(property.Name))
+                .ToList();
+
+            var rows = new List<List<String>>();
+
+            // Add columns with extracted headers
+            //var table = new ConsoleTable(columnHeaders.ToArray());
+
+
+            // Add data rows
+            for (int i = 0; i < instances.Count; i++)
+            {
+                var rowData = properties
+                    .Where(property => property.CanRead && !property.GetMethod.IsStatic)
+                    .Select(property =>
+                    {
+                        var propertyValue = property.GetValue(instances[i]);
+
+                        return propertyValue == null ? "" : propertyValue.ToString();
+                    })
+                    .ToList();
+
+                rows.Add(rowData);
+            }
+
+            ConsoleTable table;
+
+            if (shouldEnumerate)
+            {
+                table = GetEnumeratedTable(columns, rows);
+            }
+            else
+            {
+                table = new ConsoleTable(columns.ToArray());
+
+                rows.ForEach(row => table.AddRow(row.ToArray()));
+            }
+
+            table.Options.NumberAlignment = Alignment.Right;
+
+            return table.ToString();
+        }
+
+        public static ConsoleTable GetEnumeratedTable(
+            IEnumerable<String> headers,
+            IEnumerable<IEnumerable<String>> rows
+        )
+        {
+            var columns = headers.ToList();
+            columns.Insert(0, "#");
+
+            var table = new ConsoleTable(columns.ToArray());
+
+            for (int i = 0; i < rows.Count(); i++)
+            {
+                var row = rows.ElementAt(i).ToList();
+                row.Insert(0, (i + 1).ToString());
+
+                table.AddRow(row.ToArray());
+            }
+
+            table.Options.EnableCount = false;
+            table.Options.NumberAlignment = Alignment.Right;
+
+            return table;
+        }
+
+        public static string ReadValidatedValue(
+            Predicate<string>? ValidateValue,
+            Action? OnValidationFailed
+        )
+        {
+            string result;
+
+            while (true)
+            {
+                Write("--> ");
+                var input = ReadLine() ?? "";
+
+                if (ValidateValue != null && ValidateValue(input))
+                {
+                    result = input;
+                    break;
+                }
+
+                OnValidationFailed?.Invoke();
+            }
+
+            return result;
+        }
+
+        public static string ReadValidatedValueWithLabel(
+            string label,
+            Predicate<string>? ValidateValue,
+            Action? OnValidationFailed
+        )
+        {
+            WriteLine(label);
+
+            var validatedValue = ReadValidatedValue(ValidateValue, OnValidationFailed);
+
+            return validatedValue;
+        }
+
+        public static T ReadEnumValue<T>()
+            where T : Enum
+        {
+            var enumNames = Enum.GetNames(typeof(T)).Select(TextFormatter.SplitCamelCase).ToList();
+
+            var enumDisplayName = TextFormatter.SplitCamelCase(typeof(T).Name);
+
+            var enumTable = GetEnumeratedTable(
+                new List<String>() { enumDisplayName },
+                enumNames.Select(enumName => new List<String>() { FormatRow(enumName) })
+            );
+            enumTable.Options.EnableCount = false;
+
+            var enumNum = int.Parse(
+                ReadValidatedValueWithLabel(
+                    $"Select {enumDisplayName.ToLower()}: \n\n{enumTable}",
+                    enumNum => int.Parse(enumNum) > 0 && int.Parse(enumNum) <= enumNames.Count,
+                    () => WriteLine("Invalid index. Please try again.")
+                )
+            );
+
+            var enumValueAsString = UnformatRow(enumNames[enumNum - 1]);
+
+            var enumType = typeof(T);
+
+            T enumValue = (T)Enum.Parse(enumType, enumValueAsString);
+
+            return enumValue;
+        }
+
+        public static string FormatHeader(string header)
+        {
+            return TextFormatter.SplitCamelCase(header).ToLower();
+        }
+
+        public static string FormatRow(string row)
+        {
+            return TextFormatter.SplitCamelCase(row);
+        }
+
+        public static string UnformatRow(string row)
+        {
+            return TextFormatter.UnsplitCamelCase(row);
+        }
+    }
+}
--- Lab 1/ATM/Console App/Program.cs
+++ Lab 1/ATM/Console App/Program.cs
@@ -1,2 +1,258 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console_App.Classes;
+using static System.Console;
+using ConsoleTables;
+using Class_Library;
+
+const int MAX_ATTEMPTS = 3;
+
+var mockBank = MockBank.GetMockBank();
+
+var appState = AppState.Instance;
+
+appState.Initialize(mockBank, new ConsoleNotification().SendNotification);
+
+// select atm
+WriteLine(
+    "Hello! Select ATM machine from the list (selection is done via number buttons on keyboard):"
+);
+
+WriteLine(AppConsole.GetClassListAsTable(appState.Atms, true));
+
+var atmNum = int.Parse(
+    AppConsole.ReadValidatedValue(
+        value =>
+        {
+            int atmMachineNum;
+
+            bool validAtmMachineNum =
+                int.TryParse(value, out atmMachineNum)
+                && atmMachineNum > 0
+                && atmMachineNum <= appState.Atms.Count;
+
+            return validAtmMachineNum;
+        },
+        () => WriteLine("Invalid ATM machine number. Please try again.")
+    )
+);
+
+appState.SelectAtm(appState.Atms[atmNum - 1].Id);
+
+Clear();
+
+WriteLine(AppConsole.GetClassListAsTable(new List<ATM> { appState.SelectedAtm }));
+
+// select account
+for (int attempts = 0; attempts < MAX_ATTEMPTS; attempts++)
+{
+    var cardNumber = AppConsole.ReadValidatedValueWithLabel(
+        "Input card number:",
+        Credentials.ValidateCardNumber,
+        () => WriteLine("Invalid card number or format. Please try again.")
+    );
+
+    var pin = AppConsole.ReadValidatedValueWithLabel(
+        "Input pin:",
+        Credentials.ValidatePin,
+        () => WriteLine("Invalid pin or format. Please try again. ")
+    );
+
+    try
+    {
+        appState.Authenticate(new Credentials(cardNumber, pin));
+
+        WriteLine(
+            $"Welcome, {appState.SelectedAccount.OwnerFirstName} {appState.SelectedAccount.OwnerLastName}!"
+        );
+
+        break;
+    }
+    catch (Exception e)
+    {
+        attempts++;
+
+        WriteLine(e.Message);
+
+        if (attempts == MAX_ATTEMPTS - 1)
+        {
+            WriteLine("You have exceeded the maximum number of attempts. The card is blocked.");
+            Environment.Exit(0);
+        }
+
+        WriteLine($"Attempts left: {MAX_ATTEMPTS - attempts - 1}");
+    }
+}
+
+Clear();
+
+// select operation
+
+while (true)
+{
+    WriteLine();
+
+    var operationType = AppConsole.ReadEnumValue<OperationType>();
+
+    bool shouldExit = false;
+
+    switch (operationType)
+    {
+        case OperationType.Deposit:
+            var depositAmount = decimal.Parse(
+                AppConsole.ReadValidatedValueWithLabel(
+                    "Input amount to deposit:",
+                    value => decimal.Parse(value) > 0,
+                    () => WriteLine("Invalid amount. Please try again.")
+                )
+            );
+
+            try
+            {
+                appState.Deposit(depositAmount);
+
+                WriteLine(
+                    $"Operation completed successfully. Your balance is: {appState.SelectedAccount.Balance}"
+                );
+            }
+            catch (Exception e)
+            {
+                WriteLine($"Operation failed. Details: {e.Message}");
+
+                continue;
+            }
+
+            break;
+        case OperationType.Withdraw:
+            var withdrawAmount = decimal.Parse(
+                AppConsole.ReadValidatedValueWithLabel(
+                    "Input amount to withdraw:",
+                    value => decimal.TryParse(value, out decimal _),
+                    () => WriteLine("Invalid amount. Please try again.")
+                )
+            );
+
+            try
+            {
+                appState.Withdraw(withdrawAmount);
+
+                WriteLine(
+                    $"Operation completed successfully. Your balance is: {appState.SelectedAccount.Balance}"
+                );
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+
+                WriteLine($"Operation failed. Details: {e.Message}");
+
+                continue;
+            }
+
+            break;
+        case OperationType.Transaction:
+            var transactionCardNumber = AppConsole.ReadValidatedValueWithLabel(
+                "Input card number:",
+                Credentials.ValidateCardNumber,
+                () => WriteLine("Invalid card number. Please try again.")
+            );
+
+            var amountToTransfer = decimal.Parse(
+                AppConsole.ReadValidatedValueWithLabel(
+                    "Input amount to transfer:",
+                    value => decimal.Parse(value) > 0,
+                    () => WriteLine("Invalid amount. Please try again.")
+                )
+            );
+
+            try
+            {
+                appState.Transaction(transactionCardNumber, amountToTransfer);
+
+                WriteLine(
+                    $"Operation completed successfully. Your balance is: {appState.SelectedAccount.Balance}"
+                );
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+
+                WriteLine($"Operation failed. Details: {e.Message}");
+
+                continue;
+            }
+
+            break;
+        case OperationType.NearestAtms:
+            var nearestAtms = appState.GetNearestAtms();
+
+            if (nearestAtms == null)
+            {
+                WriteLine("No nearest ATMs found.");
+
+                continue;
+            }
+
+            WriteLine(AppConsole.GetClassListAsTable(nearestAtms));
+
+            break;
+        case OperationType.Exit:
+            shouldExit = true;
+
+            appState.Exit();
+
+            break;
+        case OperationType.Balance:
+            WriteLine($"Your balance is {appState.SelectedAccount.Balance}");
+
+            break;
+        case OperationType.OperationsHistory:
+            var period = AppConsole.ReadEnumValue<Period>();
+
+            List<Operation> operations;
+
+            switch (period)
+            {
+                case Period.Today:
+                    operations = appState.SelectedAccount.Operations
+                        .Where(operation => operation.Timestamp.Date == DateTime.Now.Date)
+                        .ToList();
+
+                    break;
+                case Period.ThisWeek:
+                    operations = appState.SelectedAccount.Operations
+                        .Where(
+                            operation => operation.Timestamp.Date >= DateTime.Now.Date.AddDays(-7)
+                        )
+                        .ToList();
+
+                    break;
+                case Period.ThisMonth:
+                    operations = appState.SelectedAccount.Operations
+                        .Where(
+                            operation =>
+                                operation.Timestamp.Date >= DateTime.Now.Date.AddMonths(-1).Date
+                        )
+                        .ToList();
+
+                    break;
+                default:
+                    operations = appState.SelectedAccount.Operations;
+
+                    break;
+            }
+
+            WriteLine($"{TextFormatter.SplitCamelCase(period.ToString())}'s operations:");
+
+            WriteLine(AppConsole.GetClassListAsTable(operations));
+
+            break;
+        default:
+            WriteLine("Option is not available.");
+
+            break;
+    }
+
+    if (shouldExit)
+    {
+        break;
+    }
+}
